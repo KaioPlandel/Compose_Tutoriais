1533f061fc0125af3895f89c54208e9dd126ba46
kaioc <kaioplandel@gmail.com>
2022-09-07 21:23:27 -0300

    add constraintLayout

---
+++ app/src/main/java/com/plandel/composetutoriais/ConstraintLayout.kt
@@ -0,0 +1,176 @@
+package com.plandel.composetutoriais
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+
+@Composable
+fun Constraint() {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+    ) {
+        val (textTopStart, textTopCenter, textTopEnd, textCenterStart, textCenter, textCenterEnd, textBottomStart, textBottomCenter, textBottomEnd) = createRefs()
+        Text(text = "Text Top Start", Modifier.constrainAs(textTopStart) {
+            start.linkTo(parent.start, 8.dp)
+            top.linkTo(parent.top, 8.dp)
+        })
+        Text(text = "Text Top Center", Modifier.constrainAs(textTopCenter) {
+            start.linkTo(parent.start, 8.dp)
+            top.linkTo(parent.top, 8.dp)
+            end.linkTo(parent.end, 8.dp)
+        })
+        Text(text = "Text Top End", modifier = Modifier.constrainAs(textTopEnd) {
+            top.linkTo(parent.top, 8.dp)
+            end.linkTo(parent.end, 8.dp)
+        })
+        Text(text = "Text Center Start", modifier = Modifier.constrainAs(textCenterStart) {
+            top.linkTo(textTopStart.bottom, 12.dp)
+            start.linkTo(parent.start, 8.dp)
+        })
+        Text(text = "Text Center", modifier = Modifier.constrainAs(textCenter) {
+            top.linkTo(textTopCenter.bottom, 12.dp)
+            start.linkTo(parent.start, 8.dp)
+            end.linkTo(parent.end, 8.dp)
+        })
+        Text(text = "Text Center End", modifier = Modifier.constrainAs(textCenterEnd) {
+            top.linkTo(textTopEnd.bottom, 12.dp)
+            end.linkTo(parent.end, 8.dp)
+        })
+        Text(text = "Text Bottom Start", modifier = Modifier.constrainAs(textBottomStart) {
+            top.linkTo(textCenterStart.bottom, 12.dp)
+            start.linkTo(parent.start, 8.dp)
+        })
+        Text(text = "Text Bottom Center", modifier = Modifier.constrainAs(textBottomCenter) {
+            top.linkTo(textCenter.bottom, 12.dp)
+            start.linkTo(parent.start, 8.dp)
+            end.linkTo(parent.end, 8.dp)
+        })
+        Text(text = "Text Bottom End", modifier = Modifier.constrainAs(textBottomEnd) {
+            top.linkTo(textCenterEnd.bottom, 12.dp)
+            end.linkTo(parent.end, 8.dp)
+        })
+    }
+}
+
+@Composable
+fun ConstraintWithSet(constraintSet: ConstraintSet) {
+    ConstraintLayout(
+        constraintSet,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+    ) {
+        val context = LocalContext.current
+        var userName by remember {
+            mutableStateOf("")
+        }
+        var password by remember {
+            mutableStateOf("")
+        }
+        TextField(value = userName, onValueChange = {
+            userName = it
+        }, label = { Text(text = "Username") }, modifier = Modifier.layoutId("inputName"))
+
+        TextField(value = password, onValueChange = {
+            password = it
+        }, label = { Text(text = "Password", color = Color.Red)}, visualTransformation = PasswordVisualTransformation(), modifier = Modifier.layoutId("inputPassword"))
+
+        Button(onClick = { Toast.makeText(context, "Username $userName , Password: $password ", Toast.LENGTH_SHORT).show()}, modifier = Modifier.layoutId("buttonLogin")) {
+            Text(text = "Login", fontWeight = FontWeight.Bold, color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun TitleOfApp(title: String) {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Red,
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily.Monospace
+                )
+            ) {
+                append(text = title)
+            }
+        }, modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun ConstraintLayoutGuideLine() {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .background(Color.Gray)
+            .padding(12.dp)
+    ) {
+        val (input) = createRefs()
+        val guideLineFromTop = createGuidelineFromTop(0.2f) // aqui tem 20% de distancia no top
+        val guideLineFromStart = createGuidelineFromStart(50.dp)
+        Text(text = "GuildLine From Top and Start", modifier = Modifier.constrainAs(input) {
+            top.linkTo(guideLineFromTop,8.dp)
+            start.linkTo(guideLineFromStart, 8.dp)
+            end.linkTo(parent.end, 8.dp)
+        })
+    }
+}
+
+@Composable
+fun BarrierExample() {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.Green)
+            .padding(12.dp)
+    ) {
+        val (input1, input2, input3) = createRefs()
+        val barrier = createEndBarrier(input1, input2)
+        Text(text = "Input 1 Large", modifier = Modifier.constrainAs(input1) {
+            top.linkTo(parent.top, 8.dp)
+            start.linkTo(parent.start, 8.dp)
+        })
+        Text(text = "Input 2", modifier = Modifier.constrainAs(input2) {
+            top.linkTo(input1.bottom, 8.dp)
+            start.linkTo(parent.start,8.dp)
+        })
+        Text(text = "Input 3", Modifier.constrainAs(input3) {
+            top.linkTo(input1.bottom, 8.dp)
+            start.linkTo(barrier,8.dp)
+        })
+
+    }
+}
--- app/src/main/java/com/plandel/composetutoriais/MainActivity.kt
+++ app/src/main/java/com/plandel/composetutoriais/MainActivity.kt
@@ -3,40 +3,79 @@ package com.plandel.composetutoriais
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintSet
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                DisplaySliderWithStepper()
-                DisplaySlider()
-                DisplayLinearProgressBarTwo()
-                DisplayLinearProgressBar()
-                DisplayCircularProgressBarTwo()
-                DisplayCircularProgressBar()
-                DisplaySurface()
-                DiplaySwitch()
-                DisplayRadioButton()
-                DisplayOutlineButton()
-//                DisplayPrimaryButton()
-//                DisplayPassword()
-//                DisplayTextField()
-//                DisplayOutlinedTextField()
+
+            val constraintSet = ConstraintSet {
+                val buttonLogin = createRefFor("buttonLogin")
+                val inputName = createRefFor("inputName")
+                val inputPassword = createRefFor("inputPassword")
+
+                constrain(inputName){
+                    top.linkTo(parent.top, 16.dp)
+                    start.linkTo(parent.start, 16.dp)
+                    end.linkTo(parent.end, 16.dp)
+                }
+
+                constrain(inputPassword){
+                    top.linkTo(inputName.bottom, 8.dp)
+                    start.linkTo(parent.start, 16.dp)
+                    end.linkTo(parent.end, 16.dp)
+                }
+
+                constrain(buttonLogin) {
+                    top.linkTo(inputPassword.bottom, 16.dp)
+                    start.linkTo(parent.start, 16.dp)
+                    end.linkTo(parent.end, 16.dp)
+                }
             }
 
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.White,
+                                Color.Black
+                            ), startY = 300F
+                        )
+                    )
+                    .padding(8.dp)
+            ) {
+                TitleOfApp(title = "Jetpack Compose")
+                ConstraintLayoutGuideLine()
+                Text(text = "Barrier Example", color = Color.Black, fontWeight = FontWeight.Bold, fontSize = 22.sp)
+                BarrierExample()
+                Constraint()
+                ConstraintWithSet(constraintSet = constraintSet)
+
+            }
         }
     }
 }
+
+
+
+
+
+
 
 
